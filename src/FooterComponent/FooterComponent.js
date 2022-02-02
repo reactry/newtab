eadf696af61ad70e9621306190a562350f47fcf0
Ankur Seth <iankurseth@gmail.com>
2022-02-03 04:19:17 +0530

    Hid Header and Footer

--- src/FooterComponent/FooterComponent.js
+++ src/FooterComponent/FooterComponent.js
@@ -3,7 +3,7 @@
 
 function FooterComponent () {
 	return (
-		<div className="FooterComponent bg-red-700 text-white p-12">
+		<div className="FooterComponent bg-red-700 text-white p-12 hidden">
 			<h4 className="text-xl py-12 text-center">© Copyright 2022, Ankur Seth (<a href="https://github.com/iaseth" className="text-blue-300 p-1">GitHub</a>).</h4>
 		</div>
 	);
--- src/HeaderComponent/HeaderComponent.js
+++ src/HeaderComponent/HeaderComponent.js
@@ -3,8 +3,8 @@
 
 function HeaderComponent () {
 	return (
-		<div className="HeaderComponent bg-red-500 text-white p-8">
-			<h1 className="text-center text-2xl">Reactry's Newtab</h1>
+		<div className="HeaderComponent bg-red-500 text-white p-4 hidden">
+			<h1 className="text-center text-xl">Reactry's Newtab</h1>
 		</div>
 	);
 }
