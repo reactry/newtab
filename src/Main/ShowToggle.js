7ced535bb456610c643c20d74df65284f3d7df04
Ankur Seth <iankurseth@gmail.com>
2022-02-03 17:48:02 +0530

    Now ShowToggle can be toggled with spacebar

--- src/Main/ShowToggle.js
+++ src/Main/ShowToggle.js
@@ -8,9 +8,17 @@ export default function ShowToggle ({
 	let circleClass = "h-5 w-5 -translate-y-1 rounded-full duration-300";
 	circleClass += show ? " bg-green-600 translate-x-7" : " bg-gray-600";
 
+	function handleKeyPress (e) {
+		if (e.keyCode === 32) {
+			setShow(!show);
+		}
+	}
+
 	return (
 		<div className="ShowToggle px-2 py-2 flex">
-			<div className="px-2 py-3 cursor-pointer rounded-md duration-300 hover:bg-gray-200" onClick={(e) => setShow(!show)}>
+			<div className="px-2 py-3 cursor-pointer rounded-md duration-300 hover:bg-gray-200 focus:bg-gray-200"
+				onClick={(e) => setShow(!show)} tabIndex="0"
+				onKeyDown={handleKeyPress}>
 				<div className="bg-gray-400 w-12 h-3 rounded-full">
 					<div className={circleClass}></div>
 				</div>
