f6e5e0b52ba4999f65771c1513f40e90d6e422a4
Ankur Seth <iankurseth@gmail.com>
2022-02-03 12:41:08 +0530

    Moved Main component to separate directory * things are about to get messy

--- src/Main/ShowToggle.js
+++ src/Main/ShowToggle.js
@@ -5,13 +5,13 @@ export default function ShowToggle ({
 	title, show, setShow
 }) {
 
-	let circleClass = "h-5 w-5 -translate-y-1 rounded-full";
+	let circleClass = "h-5 w-5 -translate-y-1 rounded-full duration-300";
 	circleClass += show ? " bg-green-600 translate-x-7" : " bg-gray-600";
 
 	return (
 		<div className="ShowToggle px-2 py-2">
 			<h2 className="text-sm font-bold">{title}</h2>
-			<div className="px-2 py-2">
+			<div className="px-2 py-3">
 				<div className="bg-gray-400 w-12 h-3 rounded-full cursor-pointer" onClick={(e) => setShow(!show)}>
 					<div className={circleClass}></div>
 				</div>
