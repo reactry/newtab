64180b95e1ea5507b95329a96ab74187739bf212
Ankur Seth <iankurseth@gmail.com>
2022-02-03 20:50:30 +0530

    Added rel="noreferrer"

--- src/Main/Shortcut.js
+++ src/Main/Shortcut.js
@@ -9,7 +9,7 @@ export default function Shortcut ({
 
 	return (
 		<div className="Shortcut inline-block p-2">
-			<a href={href} target="_blank"
+			<a href={href} target="_blank" rel="noreferrer"
 				className="block px-3 pt-3 pb-2 bg-gray-200 w-32 border-2 hover:text-blue-800 hover:border-blue-800 focus:text-blue-800 focus:border-blue-800 outline-none">
 				{showBigLetter && <div className="text-4xl py-4 bg-slate-300 mb-2">{firstLetter}</div>}
 				<div className="font-bold text-sm">{title}</div>
