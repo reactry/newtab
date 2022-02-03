94d7595f5db58cea1c59e8e1aef1e91b907ceb2e
Ankur Seth <iankurseth@gmail.com>
2022-02-03 20:34:38 +0530

    Corrected ShortCut's focus state

--- src/Main/Shortcut.js
+++ src/Main/Shortcut.js
@@ -10,7 +10,7 @@ export default function Shortcut ({
 	return (
 		<div className="Shortcut inline-block p-2">
 			<a href={href} target="_blank"
-				className="block px-3 pt-3 bg-gray-200 w-32 border-2 hover:text-blue-800 hover:border-blue-800">
+				className="block px-3 pt-3 bg-gray-200 w-32 border-2 hover:text-blue-800 hover:border-blue-800 focus:text-blue-800 focus:border-blue-800 outline-none">
 				<div className="text-4xl py-4 bg-slate-300">{firstLetter}</div>
 				<div className="font-bold text-sm py-2">{title}</div>
 			</a>
